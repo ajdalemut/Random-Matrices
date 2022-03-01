97532f45bd113b677aca07c3095e50fb67c396ab
ajdalemut <42772549+ajdalemut@users.noreply.github.com>
2022-03-01 18:35:59 +0100

    nekaj popravkov v osnovnih funkcijah

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Random-Matrix
+# Random-Matrices
+## Projektna naloga za predmet Matematika z racunalnikom
+
+**I observe random matrices with elements generated as:**
+* uniformly distributed random numbers in the interval (0,1)
+* uniformly distributed random integers in the range defined by r
+* normally distributed random numbers
--- analiza.m
+++ analiza.m
@@ -1,6 +1,6 @@
 
 n = 5;
-st_ponovitev = 100;
+st_ponovitev = 300;
 
 [stevilo_realnih_lastnih, vse_lastne, vse_sledi, vse_det] = fanaliza(n, st_ponovitev);
 
@@ -10,17 +10,13 @@ povprecje_st_realnih = mean(stevilo_realnih_lastnih);
 % Graf lastnih vrednosti
 figure
 plot(real(vse_lastne), imag(vse_lastne),'.')
+title('Graf vseh lastnih vrednosti v kompleksni ravnini')
+% savefig('vselastne_uni')
+% savefig('vselastne_norm')
+savefig('vselastne_r')
 
-%graf sledi 
-figure
-histogram(vse_sledi)
-
-%graf determinant
-figure
-histogram(vse_det)
-
-%graf deleža realnih lastnih vrednosti
-maxdim=1000;
+% Graf deleža realnih lastnih vrednosti
+maxdim=100;
 x = 1:1:maxdim;
 y = zeros(1,maxdim);
 for i = x
@@ -28,7 +24,29 @@ for i = x
     povprecje_st_realnih = mean(stevilo_realnih_lastnih);
     y(i) =  povprecje_st_realnih/i; %delež realnih
 end
+figure
 plot(x,y,'.')
+title('Graf deleza realnih lastnih vrednosti v odvisnosti od velikosti matrike')
+xlabel('Velikost matrike')
+ylabel('Delez realnih lastnih vrednosti')
+% savefig('delez_uni')
+% savefig('delez_norm')
+savefig('delez_r')
 
+% Graf sledi 
+figure
+histogram(vse_sledi)
+title('Graf vseh sledi v kompleksni ravnini')
+% savefig('sledi_uni')
+% savefig('sledi_norm')
+savefig('sledi_r')
+
+% Graf determinant
+figure
+histogram(vse_det)
+title('Graf vseh determinant v kompleksni ravnini')
+% savefig('det_uni')
+% savefig('det_norm')
+savefig('det_r')
 
 
